aeea41a4f610560c1b960ee377717b7a0137fc33
spogoretskyi <spogoretskyi@gmail.com>
2023-12-21 17:32:43 +0200

    add changes

--- Task_1.py
+++ Task_1.py
@@ -1,27 +1,58 @@
+import argparse
 from pathlib import Path
+import shutil
 
-# ANSI escape codes for colored output
-COLOR_BLUE = "\\033[94m"
-COLOR_RESET = "\\033[0m"
+
+COLOR_BLUE = "\033[94m"
+COLOR_RESET = "\033[0m"
 
 
 def display_tree(path: Path, indent: str = "", prefix: str = "") -> None:
     if path.is_dir():
-        # Use blue color for directories
         print(indent + prefix + COLOR_BLUE + str(path.name) + COLOR_RESET)
         indent += "    " if prefix else ""
 
-        # Get a sorted list of children, with directories last
         children = sorted(path.iterdir(), key=lambda x: (x.is_file(), x.name))
 
         for index, child in enumerate(children):
-            # Check if the current child is the last one in the directory
             is_last = index == len(children) - 1
             display_tree(child, indent, "└── " if is_last else "├── ")
     else:
         print(indent + prefix + str(path.name))
 
 
+def recursive_copy(src: Path, dest: Path):
+    dest.mkdir(parents=True, exist_ok=True)
+
+    for child in src.iterdir():
+        if child.is_dir():
+            recursive_copy(child, dest / child.name)
+        else:
+            shutil.copy2(child, dest / child.suffix[1:] / child.name)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Рекурсивно хкопійовані та відсортовані файли за їх розширенням."
+    )
+    parser.add_argument(
+        "source_directory", type=Path, help="Шлях до вихідної директорії "
+    )
+    parser.add_argument(
+        "destination_directory",
+        type=Path,
+        nargs="?",
+        default=Path("dist"),
+        help="Шлях до дирикторії призначення (default: 'dist')",
+    )
+    args = parser.parse_args()
+
+    display_tree(args.source_directory)
+
+    recursive_copy(args.source_directory, args.destination_directory)
+
+    print("Файли зкопійовані та відсортовані успішно!")
+
+
 if __name__ == "__main__":
-    root = Path("picture")
-    display_tree(root)
+    main()
